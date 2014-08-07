826ee4c0640df36f303631729d53b9479d0038bb
C <fusaix.c@gmail.com>
2014-08-07 19:19:40 -0400

    ...

---
+++ BarcodeFitness-iPhone/BFSet.m
@@ -0,0 +1,38 @@
+//
+//  BFSet.m
+//  BarcodeFitness-iPhone
+//
+//  Created by Louis CHEN on 7/24/14.
+//  Copyright (c) 2014 FrienchFriesApps. All rights reserved.
+//
+
+#import "BFSet.h"
+
+@implementation BFSet
+
+@synthesize reps = _reps;
+@synthesize weight = _weight;
+@synthesize previousReps = _previousReps;
+@synthesize previousWeight = _previousWeight;
+@synthesize setNumber = _setNumber;
+@synthesize isDone = _isDone; 
+
+
+- (id)initWithReps:(int)reps andWeight: (int)weight andPreviousReps: (int)previousReps andPreviousWeight: (int)previousWeight andSetNumber: (int)setNumber{
+    self = [super init];
+    if (self) {
+        self.reps = [[NSNumber alloc] initWithInt: reps];
+        self.weight = [[NSNumber alloc] initWithInt: weight];
+        
+        self.previousReps = [[NSNumber alloc] initWithInt: previousReps];
+        self.previousWeight = [[NSNumber alloc] initWithInt: previousWeight];
+        
+        self.setNumber = [[NSNumber alloc] initWithInt: setNumber];
+        
+        _isDone = NO;
+        
+    }
+    return self;
+}
+
+@end
